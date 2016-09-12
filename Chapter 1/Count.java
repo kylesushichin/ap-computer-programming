1cd50b624303b123ecfb0fd2f401d5d65cb939bb
kylesushichin <kyle.chin@homail.com>
2016-09-12 12:32:09 +0000

    Adding chapter 1/Count.java

---
+++ Chapter 1/Count.java
@@ -0,0 +1,17 @@
+//*****************************************************************************************************
+//Kyle Chin
+//Count.java
+//counts from 1-5 in 3 languages
+/*********************************************************/
+public class Count
+{
+    public static void main (String[] args)
+    {
+        //Language of the Burgers
+        System.out.println ("one two three four five");
+        //Language of the Fries
+        System.out.println ("un deux trois quatre cinq");
+        //Language of the Burritos
+        System.out.println ("uno dos tres cuatro cinco");
+    }
+} 
